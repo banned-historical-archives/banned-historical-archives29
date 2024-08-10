cccdfcc706fe195d605720bad12b17001b049073
Your Name <you@example.com>
2024-08-10 20:08:56 +0800

    1

---
+++ f112c5a3-f037-43b5-8aa8-a8c06fae4f0c.ts
@@ -0,0 +1,58 @@
+export default {
+  "entity": {
+    "id": "f112c5a3-f037-43b5-8aa8-a8c06fae4f0c",
+    "name": "李德生同志在全军整党建党工作座谈会上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "李德生",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives29/main/f112c5a3-f037-43b5-8aa8-a8c06fae4f0c/19.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "李德生同志在全军整党建党工作座谈会上的讲话",
+        "authors": [
+          "李德生"
+        ],
+        "page_start": 1,
+        "page_end": 19,
+        "dates": [
+          {
+            "year": 1970,
+            "month": 4
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "f112c5a3-f037-43b5-8aa8-a8c06fae4f0c",
+  "resource_type": "book",
+  "version": 2
+}
